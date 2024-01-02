39d9b3953469dfb1f9831dc1bf9cb33668255788
Bruno Souza <brunoezully@gmail.com>
2024-01-02 01:27:10 -0300

    rename component with Portfolio component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Encryption from "@/components/Main/Encryption";
-import Hero from "@/components/Main/Hero";
+import Portfolio from "@/components/Main/Portfolio";
 import Projects from "@/components/Main/Projects";
 import Skills from "@/components/Main/Skills";
 
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <main className="h-full w-full">
       <div className="flex flex-col gap-20">
-        <Hero />
+        <Portfolio />
         <Skills />
         <Encryption />
         <Projects />
