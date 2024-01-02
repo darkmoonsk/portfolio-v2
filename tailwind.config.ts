05af8c5062b2ce8ab9891887f29063cc8cbef9ba
Bruno Souza <brunoezully@gmail.com>
2024-01-02 18:58:59 -0300

    feat: add border animation to tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,16 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      keyframes: {
+        borderAnimation: {
+          "to": {
+            transform: "rotate(360deg)",
+          }
+        },        
+      },
+      animations: {
+        "animate-border": "borderAnimation 4s infinite",
+      },
     },
   },
   plugins: [],
