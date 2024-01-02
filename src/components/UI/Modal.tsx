0dc15f7f46b0a00064c4204fc3ff1001dcb11e3e
Bruno Souza <brunoezully@gmail.com>
2024-01-02 18:58:45 -0300

    feat: `Modal` component

---
+++ src/components/UI/Modal.tsx
@@ -0,0 +1,67 @@
+import Image from "next/image";
+
+interface ModalProps {
+  onClose: () => void;
+}
+
+function Modal({ onClose }: ModalProps) {
+  return (
+    <>
+    <div onClick={onClose} className="fixed w-full h-full bg-black/40 z-40"></div>
+    <div
+      className="
+    w-full max-w-[800px] max-h-[500px] overflow-y-scroll fixed left-1/2 top-[55%] -translate-x-1/2 -translate-y-1/2 shadow-lg shadow-[#2A0E61]/50
+    bg-[#03001417] backdrop-blur-md z-50 px-10 mx-3 text-white flex flex-col gap-2 p-4
+    "
+    >
+      <div>
+        <h2 className="text-2xl font-bold">Sobre mim:</h2>
+        <Image
+          src="/bruno.png"
+          alt="Foto de Bruno Souza"
+          width={200}
+          height={200}
+          className="absolute hidden md:inline right-5 top-5 rounded-full"
+        />
+      </div>
+      <div className="flex-1 flex flex-col font-semibold drop-shadow-lg gap-2 md:max-w-[75%]">
+        <p>
+          Desde muito jovem sou impulsionado por uma intensa curiosidade e uma
+          dedicação ao mundo do desenvolvimento de software e a ciência. Minha
+          paixão pela tecnologia começou com o desenvolvimento de jogos, um
+          campo no qual me aventurei profundamente como freelancer, trabalhando
+          em projetos inovadores de jogos indie.
+        </p>
+        <p>
+          Atualmente, sou um desenvolvedor fullstack, com foco em ReactJS e
+          NodeJS, e estou sempre buscando me aprimorar e aprender novas
+          tecnologias.
+        </p>
+        <p>
+          Minhas contribuições para os projetos em que participei incluem,
+          criação de sistemas de inteligência artificial avançados (Jogos) até a
+          geração de mapas procedurais (Jogos), o desenvolvimento de interfaces
+          intuitivas e responsivas (Web e Jogos) e o desenvolvimento de sistemas
+          de backend robustos e escaláveis (Web).
+        </p>
+        <p>
+          Meu objetivo é me tornar uma referencia técnica na área de dev. de
+          software, além disso mantenho uma paixão viva por Astronomia e
+          Inteligência Artificial, áreas nas quais invisto tempo em pesquisa e
+          colaboração, sempre buscando expandir e compartilhar conhecimento 
+          por diversos meios, 
+          como o <a href="https://www.brunosouzadev.com/" className="text-xl font-bold underline transition-all duration-200 ease-in-out hover:text-slate-300">meu blog</a>.
+        </p>
+      </div>
+      <button
+        className="button-primary w-full mt-2 max-w-[200px] p-3 self-center"
+        onClick={onClose}
+      >
+        Fechar
+      </button>
+    </div>
+    </>
+  );
+}
+
+export default Modal;
