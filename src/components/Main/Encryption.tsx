f4d9c1576dc14fefd4539b4901dc597ba4cd6f61
Bruno Souza <brunoezully@gmail.com>
2024-01-02 01:28:47 -0300

    feat `Encryption` component

---
+++ src/components/Main/Encryption.tsx
@@ -0,0 +1,70 @@
+"use client"
+import { slideInFromTop } from "@/utils/motion"
+import { motion } from "framer-motion"
+import Image from "next/image"
+
+function Encryption() {
+  return (
+    <div className="flex flex-row relative items-center justify-center min-h-screen w-full h-full">
+      <div className="absolute w-auto h-auto top-0 z-[5]">
+        <motion.div
+          variants={slideInFromTop}
+          className="text-5xl font-medium text-center text-gray-200"
+        >
+          Perfomance
+          <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
+          {" "}&{" "}
+          </span>
+          Segurança
+        </motion.div>
+      </div>
+      <div className="
+        flex flex-col items-center justify-center translate-y-14 absolute z-20 w-auto h-auto
+      ">
+        <Image 
+          src="/LockTop.png"
+          alt="Lock top"
+          width={50}
+          height={50}
+          className="translate-y-5 transition-all duration-200 group-hover:translate-y-11"
+        />
+        <Image 
+          src="/LockMain.png"
+          alt="Lock Main"
+          width={70}
+          height={70}
+          className="z-10"
+        />
+      </div>
+
+      <div className="
+        welcome-box px-3 py-2 my-5 z-20 border border-[#7042f88b] opacity-[0.9
+      ">
+        <h1 className="
+          welcome-text text-lg text-center text-white
+        ">Encryption</h1>
+      </div>
+      <div className="absolute z-20 bottom-3 px-2">  
+        <div className="
+          cursive text-2xl font-medium text-center text-gray-300
+        ">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </div>
+      </div>
+
+      <div className="w-full flex items-start justify-center absolute">
+        <video 
+          muted
+          autoPlay
+          loop
+          playsInline
+          preload="false"
+          className="w-full h-auto"
+          src="/encryption.webm"
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Encryption
