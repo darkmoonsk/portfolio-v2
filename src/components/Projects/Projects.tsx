e75e569fb9f4bd362b1f8e32a3eda152061ec342
Bruno Souza <brunoezully@gmail.com>
2024-03-23 21:03:49 -0300

    feat: update Projects component layout and add new project card: `Quantic Learn`

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -2,12 +2,13 @@ import ProjectCard from "./ProjectCard";
 
 function Projects() {
   return (
-    <div className="
+    <div
+      className="
       flex flex-col items-center justify-center mt-5 mb-32 z-[20]
     "
-    id="projects"
+      id="projects"
     >
-      <video 
+      <video
         autoPlay
         muted
         loop
@@ -15,37 +16,56 @@ function Projects() {
       >
         <source src="/videos/blackhole.webm" type="video/webm" />
       </video>
-      <h1 className="
+      <h1
+        className="
         text-5xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20
-      ">
+      "
+      >
         Meus Projetos
       </h1>
-      <div className="
+      <div
+        className="
         h-full w-full grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-10 gap-y-20 px-4 items-center justify-center
-      ">
-        <ProjectCard 
+      "
+      >
+        <ProjectCard
+          src="/projects/quantic-learn.png"
+          link="https://quanticlearn.com/"
+          title="Quantic Learn - Plataforma de aprendizado livre"
+          description={[
+            "A Quantic Learn é uma plataforma online de aprendizado livre, dedicada à democratização do conhecimento por meio da tecnologia. Nascida da minha crença fundamental de que a tecnologia representa um meio poderoso para proporcionar educação de alta qualidade acessível a todos e como o meu projeto de conclusão da minha graduação em Engenharia de Software.",
+            "É um projeto comprometido com tornar o aprendizado mais acessível e eficaz, permitindo que indivíduos de todas as origens tenham a oportunidade de adquirir conhecimento de maneira transformadora. Atualmente focada no aprendizado do inglês e em breve novos idiomas e áreas do conhecimento.",
+          ]}
+        />
+        <ProjectCard
           src="/projects/dinoapi.jpg"
           link="https://dinoapi.brunosouzadev.com/"
           repo="https://github.com/darkmoonsk/dinoapi"
           title="Dinoapi - API de dados sobre dinossauros"
-          description="Uma API que fornece dados históricos sobre diversos dinossauros. Construida usando o banco de dados PaleoDB, o maior banco de dados open-source de paleontologia do mundo para obter as informações. Os dados retornados sempre serão fáceis de entender, e úteis para pessoas que não são paleontólogas.
-          "
+          description={[
+            "Uma API que fornece dados históricos sobre diversos dinossauros. Construida usando o banco de dados PaleoDB, o maior banco de dados open-source de paleontologia do mundo para obter as informações",
+            "Os dados retornados sempre serão fáceis de entender, e úteis para pessoas que não são paleontólogas.",
+          ]}
         />
-        <ProjectCard 
+        <ProjectCard
           src="/projects/rapidtalks.jpg"
           hoverSrc="/projects/videos/rapidtalks.webm"
           link="https://rapidtalks.brunosouzadev.com/"
           repo="https://github.com/darkmoonsk/rapid-talks"
           title="RapidTalks - Plataforma de chat em tempo real"
-          description="Rapid Talks é uma POC (proof of concept) de uma aplicação de chat dinâmica, interativa desenvolvida com o objetivo de realizar a comunicação em tempo real. Utilizando uma combinação de tecnologias modernas e práticas de desenvolvimento avançadas."
+          description={[
+            "Rapid Talks é uma POC (proof of concept) de uma aplicação de chat dinâmica, interativa desenvolvida com o objetivo de realizar a comunicação em tempo real. Utilizando uma combinação de tecnologias modernas e práticas de desenvolvimento avançadas.",
+          ]}
         />
-        <ProjectCard 
+        <ProjectCard
           src="/projects/blog.png"
           hoverSrc="/projects/videos/blog.webm"
           link="https://www.brunosouzadev.com/"
           repo="https://github.com/darkmoonsk/blog"
           title="Blog Moderno com Next.js e TailwindCSS"
-          description="Meu blog pessoal, feito com Next.js, TailwindCSS e Typescript, com o objetivo de compartilhar conhecimento e experiências. Ele possui a visão de CMS Headless."
+          description={[
+            "Meu blog pessoal, feito com Next.js, TailwindCSS e Typescript, com o objetivo de compartilhar conhecimento e experiências sobre programação e ciências. Ele possui a visão de CMS Headless.",
+          ]}
         />
         <ProjectCard
           src="/projects/fernanda.png"
@@ -53,7 +73,9 @@ function Projects() {
           link="https://www.fernandaschubert.com/"
           repo="https://github.com/darkmoonsk/project-nanda"
           title="Fernanda Schubert - Site de captura para professora de idiomas"
-          description="Site para uma professora de idiomas, com tradução em alemão, espanhol e português, também conta com um formulário funcional."
+          description={[
+            "Site para uma professora de idiomas, com tradução em alemão, espanhol e português, também conta com um formulário funcional.",
+          ]}
         />
         <ProjectCard
           src="/projects/ministry.png"
@@ -61,23 +83,27 @@ function Projects() {
           link="https://ministry.brunosouzadev.com/"
           repo="https://github.com/darkmoonsk/app_ministry"
           title="Ministry - Gestão de relatórios"
-          description="Aplicativo web para controle de relatórios de serviço de pregação das Testemunhas de Jeová, com autenticação, dashboard do usuário e usa o banco de dados firestore do Firebase."
+          description={[
+            "Aplicativo web para controle de relatórios de serviço de pregação das Testemunhas de Jeová, com autenticação, dashboard do usuário e usa o banco de dados firestore do Firebase.",
+          ]}
         />
-        <ProjectCard
+        {/* <ProjectCard
           src="/projects/disneyplus.png"
           hoverSrc="/projects/videos/disneyplus.webm"
           link="https://disneyplus.brunosouzadev.com/"
           repo="https://github.com/darkmoonsk/disney-plus"
           title="Front-end do Disney Plus"
-          description="Recriação de uma parte do front-end do disney plus com React e styled-components, com o objetivo de praticar o uso de animações, transições e estilização de componentes."
-        />
+          description={["Recriação de uma parte do front-end do disney plus com React e styled-components, com o objetivo de praticar o uso de animações, transições e estilização de componentes."]}
+        /> */}
         <ProjectCard
           src="/projects/hadasclima.png"
           hoverSrc="/projects/videos/hadasclima.webm"
           link="https://proyecto-hadasclima.vercel.app/"
           repo="https://github.com/darkmoonsk/project-weather"
           title="HadasClima - Informações meteorológicas"
-          description="Um web app que fornece informações meteorológicas em tempo real, consumindo a api OpenWeather. "
+          description={[
+            "Um web app que fornece informações meteorológicas em tempo real, consumindo a api OpenWeather. ",
+          ]}
         />
         <ProjectCard
           src="/projects/worldcup.png"
@@ -85,11 +111,13 @@ function Projects() {
           link="https://worldcup2022.brunosouzadev.com/"
           repo="https://github.com/darkmoonsk/world-cup-2022"
           title="World Cup 2022 - Simulador da Copa Mundo de 2022"
-          description='Um projeto que foi criado para um desafio de programação de curto prazo e que utiliza React e JavaScript para "simular" os jogos da Copa do Mundo de 2022.'
+          description={[
+            'Um projeto que foi criado para um desafio de programação de curto prazo e que utiliza React e JavaScript para "simular" os jogos da Copa do Mundo de 2022.',
+          ]}
         />
       </div>
     </div>
-  )
+  );
 }
 
 export default Projects;
