061dd520303dd93a62d809e2e686697ae73f4467
Bruno Souza <brunoezully@gmail.com>
2024-03-23 21:04:20 -0300

    refactor: ProjectCard component to support multiple descriptions, and better visual

--- src/components/Projects/ProjectCard.tsx
+++ src/components/Projects/ProjectCard.tsx
@@ -4,10 +4,10 @@ import VideoPreview from "./VideoPreview"
 
 interface ProjectCardProps {
   title: string
-  description: string
+  description: string[]
   src: string
   hoverSrc?: string
-  repo: string
+  repo?: string
   link: string
 }
 
@@ -28,15 +28,24 @@ function ProjectCard({ title, description, src, hoverSrc, link, repo }: ProjectC
         <h1 className="text-2xl font-semibold fire-text">
           {title}
         </h1>
-        <p className="mt-2 text-gray-300">{description}</p>
+        <div className="min-h-[180px] max-h-[180px] overflow-y-auto">
+          {description.map((description, index) => (
+            <p key={index} className="mt-2 text-gray-300">{description}</p>
+          ))
+
+          }
+        </div>
       </div>
       <div className="flex gap-2">
         <Link href={link} target="_blank" className="flex-1 w-full cursor-pointer py-2 text-white text-center bg-[#2A0E61] hover:bg-[#1D0B34] transition duration-200">
             Ver Site
         </Link>
-        <Link href={repo} target="_blank" className="flex-1 w-full py-2 cursor-pointer text-white text-center bg-[#2A0E61] hover:bg-[#1D0B34] transition duration-200">
-            Ver Projeto
-        </Link>
+        { repo && (
+            <Link href={repo} target="_blank" className="flex-1 w-full py-2 cursor-pointer text-white text-center bg-[#2A0E61] hover:bg-[#1D0B34] transition duration-200">
+                Ver Projeto
+            </Link>
+          )
+        }
       </div>
     </div>
   )
